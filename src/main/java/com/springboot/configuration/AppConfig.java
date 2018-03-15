010f08f03731cd59df1665a0d4e44c82faa98eee
Igor Zaporogtsev <igorzaporogtsev@gmail.com>
2018-03-15 09:02:03 +0200

    Almost end project

--- src/main/java/com/springboot/configuration/AppConfig.java
+++ src/main/java/com/springboot/configuration/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import com.springboot.configuration.initializer.TestDataInitializer;
 
@@ -21,15 +22,18 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     }
 */
 
-
-
-
-
     @Bean(initMethod = "init")
     public TestDataInitializer initTestData() {
         return new TestDataInitializer();
     }
 
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/static/**")) {
+            registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        }
+    }
 }  
 
 
